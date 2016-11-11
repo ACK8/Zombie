6f694417ca16ef3db4f77cfe211a28a4298fc786
ACK202_08 <m3568853679@neokidesu.click>
2016-11-11 12:21:34 +0900

    add Door

---
+++ Assets/Door/Scrips/Cardkey.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cardkey : MonoBehaviour
+{
+    [SerializeField]
+    private Door doorScript;
+    [SerializeField]
+    private int cardKeyID;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.tag == "Key")
+        {
+            //カードキーIDとカードIDが同じ時にドアを施錠
+            if (cardKeyID == hit.GetComponent<Key>().cardID)
+            {
+                doorScript.MoveDoor();
+            }
+        }
+    }
+}
---
+++ Assets/Door/Scrips/Door.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+    public enum openType
+    {
+        Right,
+        Left
+    }
+
+    [SerializeField]
+    private float movingDistance = 0f;
+    [SerializeField]
+    private float movingSpeed = 0f;
+    [SerializeField]
+    private openType type;
+
+    private bool isOpen = false;
+
+    void Start()
+    {
+
+    }
+
+    //CardKeyから呼ばれる
+    public void MoveDoor()
+    {
+        isOpen = !isOpen;
+    }
+
+    void Update()
+    {
+        if (isOpen)
+        {
+            OpenDoor();
+        }
+        else
+        {
+            CloseDoor();
+        }
+    }
+
+    void OpenDoor()
+    {
+        Vector3 currentPos = transform.position;
+
+        switch (type)
+        {
+            case openType.Left:
+                currentPos += new Vector3(0f, 0f, -movingSpeed);
+                currentPos.z = Mathf.Clamp(currentPos.z, -movingDistance, 0);
+                
+                break;
+
+            case openType.Right:
+                currentPos += new Vector3(0f, 0f, movingSpeed);
+                currentPos.z = Mathf.Clamp(currentPos.z, 0, movingDistance);
+
+                break;
+        }
+
+        transform.position = currentPos;
+    }
+
+    void CloseDoor()
+    {
+        Vector3 currentPos = transform.position;
+
+        switch (type)
+        {
+            case openType.Left:
+                currentPos += new Vector3(0f, 0f, movingSpeed);
+                currentPos.z = Mathf.Clamp(currentPos.z, -movingDistance, 0);
+
+                break;
+
+            case openType.Right:
+                currentPos += new Vector3(0f, 0f, -movingSpeed);
+                currentPos.z = Mathf.Clamp(currentPos.z, 0, movingDistance);
+
+                break;
+        }
+
+        transform.position = currentPos;
+    }
+}
