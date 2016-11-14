cff3342ebc4a38035349ecd929cc083ee20c321e
ACK202-07\UDON <m3568853679@neokidesu.click>
2016-11-14 10:34:34 +0900

    CameraCollision

---
+++ Assets/CameraCollision/BoxDraw.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxDraw : MonoBehaviour
+{
+    public Material lineMaterial;
+
+    private BoxCollider boxCollider;
+    private Vector3[] vertex;
+
+    public Transform m_Transform;
+
+    void Start ()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+
+        float x = boxCollider.size.x;
+        float y = boxCollider.size.y;
+        float z = boxCollider.size.z;
+
+        Vector3 center = boxCollider.center;
+
+        vertex = new Vector3[8];
+
+        vertex[0] = new Vector3(center.x + x, center.y + y, center.z + z); //右上
+        vertex[1] = new Vector3(center.x + x, center.y + y, center.z - z); //左上
+        vertex[2] = new Vector3(center.x + x, center.y - y, center.z - z); //左下
+        vertex[3] = new Vector3(center.x + x, center.y - y, center.z + z); //右下
+
+        vertex[4] = new Vector3(center.x - x, center.y + y, center.z + z); //右上
+        vertex[5] = new Vector3(center.x - x, center.y + y, center.z - z); //左上
+        vertex[6] = new Vector3(center.x - x, center.y - y, center.z - z); //左下
+        vertex[7] = new Vector3(center.x - x, center.y - y, center.z + z); //右下
+    }
+
+    void Update ()
+    {
+
+    }
+
+    void OnRenderObject()
+    {
+        lineMaterial.SetPass(0);
+
+        GL.PushMatrix();
+        GL.MultMatrix(m_Transform.localToWorldMatrix);
+        GL.Begin(GL.LINES);
+
+        //+X側面
+        {
+            //上
+            GL.Vertex3(vertex[0].x, vertex[0].y, vertex[0].z);
+            GL.Vertex3(vertex[1].x, vertex[1].y, vertex[1].z);
+
+            //下
+            GL.Vertex3(vertex[2].x, vertex[2].y, vertex[2].z);
+            GL.Vertex3(vertex[3].x, vertex[3].y, vertex[3].z);
+
+            //右
+            GL.Vertex3(vertex[0].x, vertex[0].y, vertex[0].z);
+            GL.Vertex3(vertex[3].x, vertex[3].y, vertex[3].z);
+
+            //左
+            GL.Vertex3(vertex[1].x, vertex[1].y, vertex[1].z);
+            GL.Vertex3(vertex[2].x, vertex[2].y, vertex[2].z);
+        }
+
+        //-X側面
+        {
+            //上
+            GL.Vertex3(vertex[4].x, vertex[4].y, vertex[4].z);
+            GL.Vertex3(vertex[5].x, vertex[5].y, vertex[5].z);
+
+            //下
+            GL.Vertex3(vertex[6].x, vertex[6].y, vertex[6].z);
+            GL.Vertex3(vertex[7].x, vertex[7].y, vertex[7].z);
+
+            //右
+            GL.Vertex3(vertex[4].x, vertex[4].y, vertex[4].z);
+            GL.Vertex3(vertex[7].x, vertex[7].y, vertex[7].z);
+
+            //左
+            GL.Vertex3(vertex[5].x, vertex[5].y, vertex[5].z);
+            GL.Vertex3(vertex[6].x, vertex[6].y, vertex[6].z);
+        }
+
+        //上面
+        {
+            //+Z
+            GL.Vertex3(vertex[0].x, vertex[0].y, vertex[0].z);
+            GL.Vertex3(vertex[4].x, vertex[4].y, vertex[4].z);
+
+            //-Z
+            GL.Vertex3(vertex[1].x, vertex[1].y, vertex[1].z);
+            GL.Vertex3(vertex[5].x, vertex[5].y, vertex[5].z);
+        }
+
+        //下面
+        {
+            //+Z
+            GL.Vertex3(vertex[3].x, vertex[3].y, vertex[3].z);
+            GL.Vertex3(vertex[7].x, vertex[7].y, vertex[7].z);
+
+            //-Z
+            GL.Vertex3(vertex[2].x, vertex[2].y, vertex[2].z);
+            GL.Vertex3(vertex[6].x, vertex[6].y, vertex[6].z);
+        }
+
+        GL.End();
+        GL.PopMatrix();
+    }
+}
---
+++ Assets/CameraCollision/CameraFade.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFade : MonoBehaviour
+{
+    public string areaName;
+    public Transform playerTransform;
+
+    private Transform targetTransform;
+
+    private bool isArea = true;
+    private string currentArea;
+    private LineRenderer line;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.SetVertexCount(2);
+    }
+
+    void Update()
+    {
+        if (!isArea)
+        {
+            line.SetPosition(0, targetTransform.transform.position);
+            
+            Vector3 pos = new Vector3(transform.position.x, targetTransform.transform.position.y, transform.position.z);
+            line.SetPosition(1, pos);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //エリアごとの処理
+        if (isArea)
+        {
+            //自分がいるエリアを取得
+            if (collider.transform.tag == "Area")
+            {
+                areaName = collider.transform.name;
+                Debug.Log(areaName);
+
+                targetTransform = collider.gameObject.transform.FindChild("Return");
+            }
+
+            //カメラが壁にめり込んだ
+            if (collider.transform.tag == "Wall")
+            {
+                Debug.Log(collider.transform.tag);
+
+                line.enabled = true;
+                isArea = false;
+            }
+        }
+        else
+        {
+            //カメラが指定の場所に戻った
+            if (collider.transform.name == "Return")
+            {
+                Debug.Log(collider.transform.name);
+
+                line.enabled = false;
+                isArea = true;
+            }
+        }
+    }
+}
