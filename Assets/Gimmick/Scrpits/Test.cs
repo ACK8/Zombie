1679e831a89c8deefd164810868a9bcf6da9a615
ACK202_08 <m3568853679@neokidesu.click>
2016-11-25 11:38:58 +0900

    ギミックシーン追加

---
+++ Assets/Gimmick/Scrpits/Test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+
+	void Start ()
+    {
+	
+	}
+	
+
+	void Update ()
+    {
+        print("aaaaaaaaaaaaa");
+	}
+}
