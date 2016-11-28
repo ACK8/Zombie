33786934a5fcb571b862d178701d08be2eb47a6e
GameZonbi <h.282424kome@gmail.com>
2016-11-28 14:58:15 +0900

    障害物の耐久値

---
+++ Assets/Gimmick/Scrpits/DestructionObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructionObject : MonoBehaviour {
+    
+    [SerializeField]
+    private int Hp;
+    [SerializeField]
+    private int decrease;
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void EnduranceVaule()
+    {
+        if (Hp != 0)
+        {
+            Hp -= 10;
+        }else if(Hp<=0)
+        {
+            Hp = 0;
+        }
+    }
+}
