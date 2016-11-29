f7fc9da4bbb3325959bd3a07f3486ebbc7f64949
ken60 <kenken60826@gmail.com>
2016-11-29 15:25:44 +0900

    add menu

---
+++ Assets/ZombieOperation/Scripts/System/Menu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    private string titleSceneName = null;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SelectMenu(string name)
+    {
+        switch (name)
+        {
+            case "Restart":
+                Restart();
+                break;
+
+            case "BackToTitle":
+                BackToTitle();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void Restart()
+    {
+        print("Restart");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void BackToTitle()
+    {
+        print("BackToTitle");
+        SceneManager.LoadScene(titleSceneName);
+    }
+}
