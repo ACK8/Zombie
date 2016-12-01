f22fbb2a3384ba2197661129ba56ec0ccde6f136
ACKGame <m2127693073@neokidesu.click>
2016-12-01 11:19:37 +0900

    add vrColtroller

---
+++ Assets/ZombieOperation/Scripts/Player/VRColtroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRColtroller : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedComponent;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var device = SteamVR_Controller.Input((int)trackedComponent.index);
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+
+        }
+    }
+}
